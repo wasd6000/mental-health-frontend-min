96f04b953e9103ebae5a41d748804f940da445fb
wasd6000 <wasd6000@126.com>
2026-04-15 21:43:12 +0800

    修复了一些bug

--- src/api/notice.js
+++ src/api/notice.js
@@ -1,11 +1,49 @@
 // src/api/notice.js
-import { normalizeId } from '../utils/idHelper'
+import request from './request.js'
 
+/**
+ * 获取公告列表
+ */
+export function getNoticeList(params = {}) {
+  return request.get('/api/notice/list', {
+    params: {
+      page: 1,
+      pageSize: 20,
+      ...params
+    }
+  })
+}
+
+/**
+ * 获取公告详情
+ */
 export function getNoticeDetail(id) {
   // 公告模块使用自增 ID，转换为数字
-  const normalizedId = normalizeId(id, 'notice')
+  const numericId = Number(id)
+  const normalizedId = isNaN(numericId) ? id : numericId
 
   return request.get('/api/notice/detail', {
     params: { id: normalizedId }
   })
+}
+
+/**
+ * 创建公告
+ */
+export function createNotice(data) {
+  return request.post('/api/notice', data)
+}
+
+/**
+ * 更新公告
+ */
+export function updateNotice(id, data) {
+  return request.put(`/api/notice/${id}`, data)
+}
+
+/**
+ * 删除公告
+ */
+export function deleteNotice(id) {
+  return request.delete(`/api/notice/${id}`)
 }
